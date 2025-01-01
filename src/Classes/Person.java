07f86874407ea5a96a077d5bcf90734d29d5ad18
Erkhan <erkhanpiriyev@gmail.com>
2025-01-01 18:53:26 +0500

    создал все классы

---
+++ src/Classes/Person.java
@@ -0,0 +1,48 @@
+package Classes;
+
+public abstract class Person
+{
+    private static int id_gen = 1;
+    private int id;
+    private String name;
+    private String surname;
+
+    public Person()
+    {
+        id = id_gen++;
+    }
+
+    public Person(String name , String surname)
+    {
+        this();
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public abstract String getPosition();
+
+    @Override
+    public String toString() {
+        return (id + ". " + name + " " + surname);
+    }
+}
